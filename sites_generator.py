ea6b46f3c78fdb4010fdeeb0875975c34011b6a5
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-02-08 11:42:05 +0300

    Added index and articles rendering

--- sites_generator.py
+++ sites_generator.py
@@ -1,12 +1,9 @@
 import json
 import chardet
 import markdown
-from os import mkdir
-from os.path import exists, join, splitext
+from os import makedirs
+from os.path import basename, exists, join
 from jinja2 import Environment, FileSystemLoader
-
-
-CONFIG_FILEPATH = '.\config.json'
 
 
 def load_config_file(path):
@@ -14,24 +11,11 @@ def load_config_file(path):
         return json.load(json_obj)
 
 
-def get_configs_from_file(config_json):
-    configs = list()
-    for article in config_json.get('articles'):
-        article_config = dict()
-        article_config['title'] = article.get('title')
-        article_config['source'] = article.get('source')
-        article_config['dir'] = article_config['source'].split('/')[0]
-        article_config['filename'] = article_config['source'].split('/')[1]
-        configs.append(article_config)
-    return configs
-
-#print(get_configs_from_file(load_config_file(CONFIG_FILEPATH)))
-
 def define_md_file_charset(path):
     with open(path,'rb') as md_obj:
         return chardet.detect(md_obj.read()).get('encoding')
 
-    
+
 def read_md(filepath, charset):
     with open(filepath, 'r', encoding = charset) as md_obj:
         return md_obj.read()
@@ -39,35 +23,72 @@ def read_md(filepath, charset):
 
 def convert_md_to_html(md_obj):
     extensions = ['codehilite', 'extra', 'smarty']
-    html = markdown.markdown(md_obj, extensions=extensions, output_format='html5')
-    return html
+    html_obj = markdown.markdown(
+        md_obj,
+        extensions=extensions,
+        output_format='html5'
+    )
+    return html_obj
 
 
-def render_page(data):
-    loader=FileSystemLoader('./templates', encoding='utf-8', followlinks=False)
-    env = Environment(loader) 
-    template = env.get_template('templates/{}.html'.format())
+def create_site_dirs_tree(config, site_basepath):
+    topic_dir = config['source'].split('/')[0]
+    print(join(site_basepath, topic_dir))
+    if not exists(join(site_basepath, topic_dir)):
+        makedirs(join(site_basepath, topic_dir), exist_ok=True)
+
+def render_index_page(page_template):
+    loader = FileSystemLoader('templates', followlinks=True)
+    env = Environment(loader=loader)
+    template = env.get_template('{}'.format(page_template))
     return template.render()
 
 
-def create_site_structure(current_article_config_dict):
-    if not exists('site'):
-        mkdir('site')
-    dirpath = current_article_config_dict['dir']
-    if not exists(('site/{}').format(dirpath)):
-        mkdir('site/{}'.format(dirpath))
+def render_article_page(page_template, config, html_obj):
+    loader = FileSystemLoader('templates', followlinks=True)
+    env = Environment(loader=loader)
+    template = env.get_template('{}'.format(page_template))
+    context = {
+        'html': html_obj,
+        'title': config['title'],
+        'topic': config['topic']
+    }
+    return template.render(context)
 
 
-def save_converted_html(dirpath, html_obj):
-    
+def create_index_page(site_basepath, rendered_page):
+    index_html_filepath = join(site_basepath, 'index.html')
+    with open (index_html_filepath, mode='w', encoding='utf-8') as html_file:
+        html_file.write(rendered_page)
+
+
+def create_article_page(config, site_basepath, rendered_page):
+    article_md_name = config['source'].split('/')[1]
+    article_html_name = article_md_name.replace('md', 'html')
+    topic_dir = config['source'].split('/')[0]
+    article_html_path = join(site_basepath, topic_dir, article_html_name)
+    with open (article_html_path, mode='w', encoding='utf-8') as html_file:
+        html_file.write(rendered_page)
 
 
 if __name__ == '__main__':
-    configs = get_configs_from_file(load_config_file(CONFIG_FILEPATH))
-    for article_configs in configs:
-        article_filepath = 'articles/{}'.format(article_configs.get('source'))
+    article_page_template = 'article_template.html'
+    index_page_template = 'index_template.html'
+    config_filepath = 'config.json'
+    site_basepath = r'site\articles'
+    configs = load_config_file(config_filepath)
+    rendered_index_page = render_index_page(index_page_template)
+    create_index_page('site', rendered_index_page)
+    for config in configs['articles']:
+        article_filepath = 'articles/{}'.format(config['source'])
         charset = define_md_file_charset(article_filepath)
         markdown_obj = read_md(article_filepath, charset)
         html_obj = convert_md_to_html(markdown_obj)
-        create_site_structure(article_configs)
-
+        create_site_dirs_tree(config, site_basepath)
+        rendered_article_page = render_page(
+            article_page_template,
+            config,
+            html_obj
+        )
+        create_article_page(config, site_basepath, rendered_atricle_page)
+        
