f8f119e1c63c0a19fb30896662ece66710d9087e
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-02-12 13:55:30 +0300

    Added index page rendering

--- sites_generator.py
+++ sites_generator.py
@@ -3,6 +3,7 @@ import chardet
 import markdown
 from os import makedirs
 from os.path import basename, exists, join
+from collections import defaultdict
 from jinja2 import Environment, FileSystemLoader
 
 
@@ -37,23 +38,37 @@ def create_site_dirs_tree(config, site_basepath):
     if not exists(join(site_basepath, topic_dir)):
         makedirs(join(site_basepath, topic_dir), exist_ok=True)
 
-def render_index_page(page_template):
+
+def render_index_page(page_template, configs, site_basepath):
+    unordered_content = defaultdict(list)
+    ordered_content = ordereddict(list)
+    for article in configs['articles']:
+        article_filepath = join(
+            site_basepath,
+            article['source'].replace('md', 'html')
+        )
+        unordered_content[article['topic']].append(
+            [article['title'],
+             article_filepath]
+        )
+        ordered_content = 
+    print(index_page_content)
     loader = FileSystemLoader('templates', followlinks=True)
     env = Environment(loader=loader)
     template = env.get_template('{}'.format(page_template))
-    return template.render()
+    return template.render(content = index_page_content)
 
 
 def render_article_page(page_template, config, html_obj):
     loader = FileSystemLoader('templates', followlinks=True)
     env = Environment(loader=loader)
     template = env.get_template('{}'.format(page_template))
-    context = {
+    content = {
         'html': html_obj,
         'title': config['title'],
         'topic': config['topic']
     }
-    return template.render(context)
+    return template.render(content)
 
 
 def create_index_page(site_basepath, rendered_page):
@@ -62,7 +77,7 @@ def create_index_page(site_basepath, rendered_page):
         html_file.write(rendered_page)
 
 
-def create_article_page(config, site_basepath, rendered_page):
+def create_article_page(site_basepath, rendered_page, config):
     article_md_name = config['source'].split('/')[1]
     article_html_name = article_md_name.replace('md', 'html')
     topic_dir = config['source'].split('/')[0]
@@ -77,18 +92,27 @@ if __name__ == '__main__':
     config_filepath = 'config.json'
     site_basepath = r'site\articles'
     configs = load_config_file(config_filepath)
-    rendered_index_page = render_index_page(index_page_template)
-    create_index_page('site', rendered_index_page)
+
     for config in configs['articles']:
-        article_filepath = 'articles/{}'.format(config['source'])
+        article_filepath = r'articles\{}'.format(config['source'])
         charset = define_md_file_charset(article_filepath)
         markdown_obj = read_md(article_filepath, charset)
         html_obj = convert_md_to_html(markdown_obj)
         create_site_dirs_tree(config, site_basepath)
-        rendered_article_page = render_page(
+        rendered_article_page = render_article_page(
             article_page_template,
             config,
             html_obj
         )
-        create_article_page(config, site_basepath, rendered_atricle_page)
-        
+        create_article_page(
+            site_basepath,
+            rendered_article_page,
+            config
+        )
+
+    rendered_index_page = render_index_page(
+        index_page_template,
+        configs,
+        site_basepath
+    )
+    create_index_page(r'site', rendered_index_page)
